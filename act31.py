00ff06a5c101eb18d642bc918c07901f63e8a253
Litesh823 <sanjeev.rhll@gmail.com>
2026-06-30 19:34:54 +0530

    Add files via upload

---
+++ act31.py
@@ -0,0 +1,24 @@
+temp = 25
+
+# Only if
+if temp < 10:
+    print("Very Cold")
+    print("End of if block ...")
+
+    # If - else
+if temp < 10:
+    print("Very Cold")
+else:
+    print("Not that cold..")
+
+        # if-elif ... else
+if temp < 10:
+    print("Very cold")
+elif > 10 and temp <= 25:
+    print("Moderate temperature")
+elif > 20 and temp <= 35:    
+    print("Its warm ...")
+elif > 30 and temp <= 45:    
+    print("It is hot outside...")
+else:
+    print("Unbearable Hot ...")    
